3c4c1b63683240491047fe546c62219c12d58ab9
Isakulov Tohir <isakulovtohir902@gmail.com>
2022-07-21 11:37:30 +0500

    done style

---
+++ github.js
@@ -0,0 +1,28 @@
+let g = (id) => document.getElementById(id)
+let search = g("search")
+let btn = g("btn")
+btn.addEventListener('click', () => result())
+
+
+console.log(g('btn'))
+
+window.addEventListener('keydown', e => {
+    if(e.key === 'Enter') {
+        result()
+    }
+})
+
+function result() {
+    if(search.value !== '') {
+        fetch(`https://api.github.com/users/${search.value}`)
+            .then(res => res.json())
+            .then(data => {
+                
+            })
+        .catch(err => {
+            throw(err)
+        })
+    }    
+    search.value = ''
+    search.focus()
+}
