8e294176af11ee2250140ff89b1bb42e94327fc3
Isakulov Tohir <isakulovtohir902@gmail.com>
2022-07-21 14:52:37 +0500

    almost done and some problems left

--- github.js
+++ github.js
@@ -1,69 +1,84 @@
-let g = (id) => document.getElementById(id)
-let search = g("search")
-let btn = g("btn")
-let container = g('result')
-btn.addEventListener('click', () => result())
+let g = (id) => document.getElementById(id);
+let el = (id) => document.createElement(id);
+let search = g("search");
+let btn = g("btn");
+let container = document.getElementById("container");
+btn.addEventListener("click", () => result());
 
+console.log(g("btn"));
 
-console.log(g('btn'))
-
-window.addEventListener('keydown', e => {
-    if(e.key === 'Enter') {
-        result()
-    }
-})
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    result();
+  }
+});
 
 function result() {
-    if(search.value !== '') {
-        fetch(`https://api.github.com/users/${search.value}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                let top = g('top')
-                top.className = 'top'
-                let img = g('img')
-                img.src = data.avatar_url
-                let info = g('info')
-                info.className = 'info'
-                info.appendChild(name)
-                info.appendChild(nick)
-                info.appendChild(joined)
-                top.appendChild(img)
-                top.appendChild(info)
-                let name = g('name')
-                let nick = g('nick')
-                let joined = g('joined')
-                let bio = g('bio')
-                let bioInfo = g('bio-info')
-                let follows = g('follows')
-                let repos = g('repos')
-                let repo = g('repo')
-                let repNum = g('repo-num')
-                let followers = g('followers')
-                let follower = g('follower')
-                let followerNum = g('follower-num')
-                let followings = g('followings')
-                let following = g('following')
-                let followingNums = g('follower-num')
-                let links = g('links')
-                let firstSide = g('first-side')
-                let secondSide = g('second-side')
-                let location = g('location')
-                let link = g('link')
-                let twitter = g('twitter')
-                let building = g('building')
-                let locationName = g('location-name')
-                let linkName = g('link-name')
-                let twitterName = g('twitter-name')
-                let buildingName = g('building-name')
-                let divFirst = document.querySelectorAll('#div-first')
-                let divSecond = document.querySelectorAll('#div-second')
-
-            })
-        .catch(err => {
-            throw(err)
-        })
-    }    
-    search.value = ''
-    search.focus()
+  if (search.value !== "") {
+    fetch(`https://api.github.com/users/${search.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        let a = `
+                <div class="top" id="">
+                  <img src="${data.avatar_url}" class="img" alt="github user img" />
+                  <div class="info">
+                    <h2 class="name">${data.name}</h2>
+                    <span class="nick">${data.login}</span>
+                    <p class="joined">${data.created_at}</p>
+                  </div>
+                </div>
+                <div class="bio" id="bio">
+                  <p id="bio-info">
+                    ${data.bio}
+                  </p>
+                </div>
+                <div class="follows-repos" id="follows">
+                  <div class="repo" id="repos">
+                    <p class="same" id="repo">Repos</p>
+                    <h3 class="nums"id="repo-num">${data.public_repos}</h3>
+                  </div>
+                  <div class="followers" id="followers">
+                    <p class="same" id="follower">followers</p>
+                    <h3 class="nums" id="follower-num">${data.followers}</h3>
+                  </div>
+                  <div class="followings" id="followings">
+                    <p class="same" id="following">following</p>
+                    <h3 class="nums" id="following-nums">${data.following}</h3>
+                  </div>
+                </div>
+                <div class="links">
+                  <div class="first-side" id="first-side">
+                    <div class="div-first" id="div-first">
+                      <i class="fa-solid fa-location-dot same" id="location"></i>
+                      <p class="similar" id="location-name">${data.location}</p>
+                    </div>
+                    <div class="div-first" id="div-first">
+                      <i class="fa-solid fa-link same" id="link"></i>
+                      <p class="similar" id="link-name">${data.blog}</p>
+                    </div>
+                  </div>
+                  <div class="second-side" id="second-side">
+                    <div class="div-second" id="div-second">
+                      <i class="fa-solid fa-building same" id="building"></i>
+                      <p class="similar" id="building-name">${data.company}</p>
+                    </div>
+                    <div class="div-second" id="div-second">
+                        <i class="fa-brands fa-twitter same" id="twitter"></i> 
+                        <p class="similar" id="twitter-name">${data.twitter_username}</p>
+                    </div>
+                  </div>
+                </div>
+                `;
+                let res = document.getElementById('result')
+                res.innerHTML = a
+                container.appendChild(res)
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  search.value = "";
+  search.focus();
 }
+
